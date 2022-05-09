200045067975e0f698428098fc2787e650e65e3b
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-09 23:32:20 +0700

    First Commit

---
+++ While Loop/project/main.scala
@@ -0,0 +1,12 @@
+// while loop
+var stockPrice: Int = 200000
+while(stockPrice <= 400000) {
+  println(stockPrice);
+  stockPrice = stockPrice + 100000
+}
+
+var hotelRevenue: Int = 500000;
+while(hotelRevenue >= 100000) {
+  println(hotelRevenue);
+  hotelRevenue = hotelRevenue + 200000
+}
