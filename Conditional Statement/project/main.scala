f026d8ecfd7306eb18c9a9501248986a8a218843
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-09 23:28:14 +0700

    First Commit

---
+++ Conditional Statement/project/main.scala
@@ -0,0 +1,23 @@
+// conditional statement
+var salesPrice: Int = 100000
+if(salesPrice > 50000) {
+  println("Sales Price is very expensive")
+}
+
+var spicyLevel: Int = 10
+if(spicyLevel > 5) {
+  println("Very Spicy")
+} else if(spicyLevel == 5) {
+  println("Normal")
+} else {
+  println("Not Spicy")
+}
+
+var totalSupply: Int = 500000
+if(totalSupply > 500000) {
+  println("Very Enough")
+} else if(totalSupply == 500000) {
+  println("Enough")
+} else {
+  println("Not Enough")
+}
