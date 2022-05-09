5ca71d39bd6397b4024df740d8c22adf635bd03a
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-09 23:26:15 +0700

    First Commit

---
+++ Variable Data Type/project/main.scala
@@ -0,0 +1,15 @@
+// set variable data type
+var x = 100
+x
+
+var a: Int = 12
+a
+
+var myName: String = "Willibrordus Bayu"
+myName
+
+var b: Double = 2.4
+a + b
+
+var intSum = {var m: Int = 100; var n: Double = 12.6; m * n}
+intSum
